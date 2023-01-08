8536b4b9be6f8ea6ba80a4634c010ead6b534cc0
Iyad Ahmed <iyadahmed430@gmail.com>
2023-01-08 12:44:41 +0200

    Reformat code

--- tiny_stl.cpp
+++ tiny_stl.cpp
@@ -8,139 +8,134 @@
 
 namespace STL_Mesh_IO {
 
-class Binary_File_Reader : public File_Reader {
-private:
-    FILE* m_file = NULL;
+    class Binary_File_Reader : public File_Reader {
+    private:
+        FILE *m_file = NULL;
 
-public:
-    // Make instances non-copyable
-    Binary_File_Reader(const Binary_File_Reader&) = delete;
-    Binary_File_Reader& operator=(const Binary_File_Reader&) = delete;
+    public:
+        // Make instances non-copyable
+        Binary_File_Reader(const Binary_File_Reader &) = delete;
 
-    Binary_File_Reader(FILE* file)
-    {
-        this->m_file = file;
-        if (fseek(file, 84, SEEK_SET) != 0) {
-            throw std::runtime_error("Failed to seek file");
-        }
-    }
+        Binary_File_Reader &operator=(const Binary_File_Reader &) = delete;
 
-    ~Binary_File_Reader() override
-    {
-        if (m_file) {
-            fclose(m_file);
-        }
-    }
-
-    bool read_next_triangle(Triangle* res) override
-    {
-        bool success = true;
-        success = success && (fread(res->normal, sizeof(float[3]), 1, m_file) == 1);
-        success = success && (fread(res->vertices, sizeof(float[3][3]), 1, m_file) == 1);
-        success = success && (fread(&res->attribute_byte_count, sizeof(uint16_t), 1, m_file) == 1);
-        return success;
-    }
-};
-
-class ASCII_File_Reader : public File_Reader {
-private:
-    char* m_buffer = NULL;
-    char* m_iter = NULL;
-    size_t m_buffer_size = 0;
-
-public:
-    // Make instances non-copyable
-    ASCII_File_Reader(const ASCII_File_Reader&) = delete;
-    ASCII_File_Reader& operator=(const ASCII_File_Reader&) = delete;
-
-    ASCII_File_Reader(FILE* file, size_t file_size)
-    {
-        if (fseek(file, 0, SEEK_SET) != 0) {
-            throw std::runtime_error("Failed to seek file");
+        Binary_File_Reader(FILE *file) {
+            this->m_file = file;
+            if (fseek(file, 84, SEEK_SET) != 0) {
+                throw std::runtime_error("Failed to seek file");
+            }
         }
 
-        if (file_size < 6) {
-            throw std::runtime_error("File too short");
+        ~Binary_File_Reader() override {
+            if (m_file) {
+                fclose(m_file);
+            }
         }
 
-        m_buffer_size = file_size;
-        m_iter = m_buffer = new char[file_size];
-        if (fread(m_buffer, file_size, 1, file) != 1) {
+        bool read_next_triangle(Triangle *res) override {
+            bool success = true;
+            success = success && (fread(res->normal, sizeof(float[3]), 1, m_file) == 1);
+            success = success && (fread(res->vertices, sizeof(float[3][3]), 1, m_file) == 1);
+            success = success && (fread(&res->attribute_byte_count, sizeof(uint16_t), 1, m_file) == 1);
+            return success;
+        }
+    };
+
+    class ASCII_File_Reader : public File_Reader {
+    private:
+        char *m_buffer = NULL;
+        char *m_iter = NULL;
+        size_t m_buffer_size = 0;
+
+    public:
+        // Make instances non-copyable
+        ASCII_File_Reader(const ASCII_File_Reader &) = delete;
+
+        ASCII_File_Reader &operator=(const ASCII_File_Reader &) = delete;
+
+        ASCII_File_Reader(FILE *file, size_t file_size) {
+            if (fseek(file, 0, SEEK_SET) != 0) {
+                throw std::runtime_error("Failed to seek file");
+            }
+
+            if (file_size < 6) {
+                throw std::runtime_error("File too short");
+            }
+
+            m_buffer_size = file_size;
+            m_iter = m_buffer = new char[file_size];
+            if (fread(m_buffer, file_size, 1, file) != 1) {
+                fclose(file);
+                throw std::runtime_error("Failed to read from file");
+            }
             fclose(file);
-            throw std::runtime_error("Failed to read from file");
         }
-        fclose(file);
-    }
 
-    ~ASCII_File_Reader() override
-    {
-        if (m_buffer) {
-            delete[] m_buffer;
+        ~ASCII_File_Reader() override {
+            if (m_buffer) {
+                delete[] m_buffer;
+            }
         }
-    }
 
-    bool read_next_triangle(Triangle* res) override
-    {
-        int vertex_counter = 0;
-        while (m_iter < (m_buffer + m_buffer_size - 6)) {
-            if (memcmp(m_iter, "vertex", 6) == 0) {
-                m_iter += 6;
+        bool read_next_triangle(Triangle *res) override {
+            int vertex_counter = 0;
+            while (m_iter < (m_buffer + m_buffer_size - 6)) {
+                if (memcmp(m_iter, "vertex", 6) == 0) {
+                    m_iter += 6;
 
-                char* endptr = NULL;
-                // TODO: strtof error checking
-                res->vertices[vertex_counter][0] = strtof(m_iter, &endptr);
-                res->vertices[vertex_counter][1] = strtof(endptr, &endptr);
-                res->vertices[vertex_counter][2] = strtof(endptr, &endptr);
-                vertex_counter++;
+                    char *endptr = NULL;
+                    // TODO: strtof error checking
+                    res->vertices[vertex_counter][0] = strtof(m_iter, &endptr);
+                    res->vertices[vertex_counter][1] = strtof(endptr, &endptr);
+                    res->vertices[vertex_counter][2] = strtof(endptr, &endptr);
+                    vertex_counter++;
 
-                if (vertex_counter >= 3) {
-                    // TODO: verify that normal is read
-                    return true;
+                    if (vertex_counter >= 3) {
+                        // TODO: verify that normal is read
+                        return true;
+                    }
+                }
+                    // TODO: read normal vector
+                else {
+                    m_iter++;
                 }
             }
-            // TODO: read normal vector
-            else {
-                m_iter++;
-            }
+
+            return false;
+        }
+    };
+
+    std::unique_ptr<File_Reader> create_reader(const char *filepath) {
+        FILE *file = fopen(filepath, "rb");
+
+        if (!file) {
+            throw std::runtime_error("Failed to open file");
         }
 
-        return false;
+        if (fseek(file, 80, SEEK_SET) != 0) {
+            throw std::runtime_error("Failed to seek file");
+        }
+
+        uint32_t num_tris = 0;
+        if (fread(&num_tris, sizeof(uint32_t), 1, file) != 1) {
+            throw std::runtime_error("Failed to read from file");
+        }
+
+        if (fseek(file, 0, SEEK_END) != 0) {
+            throw std::runtime_error("Failed to seek file");
+        }
+
+        long file_size = ftell(file);
+
+        if (file_size == -1L) {
+            throw std::runtime_error("Failed to get file size");
+        }
+
+        assert(file_size >= 0);
+        if ((size_t) file_size == (84 + num_tris * 50)) {
+            return std::make_unique<Binary_File_Reader>(file);
+        } else {
+            return std::make_unique<ASCII_File_Reader>(file, file_size);
+        }
     }
-};
-
-std::unique_ptr<File_Reader> create_reader(const char* filepath)
-{
-    FILE* file = fopen(filepath, "rb");
-
-    if (!file) {
-        throw std::runtime_error("Failed to open file");
-    }
-
-    if (fseek(file, 80, SEEK_SET) != 0) {
-        throw std::runtime_error("Failed to seek file");
-    }
-
-    uint32_t num_tris = 0;
-    if (fread(&num_tris, sizeof(uint32_t), 1, file) != 1) {
-        throw std::runtime_error("Failed to read from file");
-    }
-
-    if (fseek(file, 0, SEEK_END) != 0) {
-        throw std::runtime_error("Failed to seek file");
-    }
-
-    long file_size = ftell(file);
-
-    if (file_size == -1L) {
-        throw std::runtime_error("Failed to get file size");
-    }
-
-    assert(file_size >= 0);
-    if ((size_t)file_size == (84 + num_tris * 50)) {
-        return std::make_unique<Binary_File_Reader>(file);
-    } else {
-        return std::make_unique<ASCII_File_Reader>(file, file_size);
-    }
-}
 
 }
--- tiny_stl.hpp
+++ tiny_stl.hpp
@@ -5,22 +5,22 @@
 
 namespace STL_Mesh_IO {
 
-struct Triangle {
-    float normal[3] {};
-    float vertices[3][3] {};
-    uint16_t attribute_byte_count = 0;
-};
+    struct Triangle {
+        float normal[3]{};
+        float vertices[3][3]{};
+        uint16_t attribute_byte_count = 0;
+    };
 
-class File_Reader {
-public:
-    virtual bool read_next_triangle(Triangle* t) = 0;
+    class File_Reader {
+    public:
+        virtual bool read_next_triangle(Triangle *t) = 0;
 
-    // NOTE: Abstract class destrctor must be virtual,
-    // otherwise, subclasses' destructors won't be called :/
-    // https://stackoverflow.com/a/25220259/8094047
-    virtual ~File_Reader() { }
-};
+        // NOTE: Abstract class destrctor must be virtual,
+        // otherwise, subclasses' destructors won't be called :/
+        // https://stackoverflow.com/a/25220259/8094047
+        virtual ~File_Reader() {}
+    };
 
-std::unique_ptr<File_Reader> create_reader(const char* filepath);
+    std::unique_ptr<File_Reader> create_reader(const char *filepath);
 
 }
