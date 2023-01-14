4940211f8d9e7d3993c0859940f847b6417ac493
Iyad Ahmed <iyadahmed430@gmail.com>
2023-01-14 08:55:22 +0200

    Format example code

--- example.cpp
+++ example.cpp
@@ -1,15 +1,15 @@
 #include <cstdio>
 #include "tiny_stl.hpp"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc != 4) {
         puts("Expected arguments: input.stl binary_output.stl ascii_output.stl");
         return 1;
     }
 
-    const char* input_filepath = argv[1];
-    const char* binary_output_filepath = argv[2];
-    const char* ascii_output_filepath = argv[3];
+    const char *input_filepath = argv[1];
+    const char *binary_output_filepath = argv[2];
+    const char *ascii_output_filepath = argv[3];
 
     auto reader = Tiny_STL::create_reader(input_filepath);
     auto binary_writer = Tiny_STL::create_writer(binary_output_filepath, Tiny_STL::File_Writer::Type::BINARY);
